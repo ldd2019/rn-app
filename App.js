282874188841056f922824298be52e74b8b97feb
spock <372842848@qq.com>
2020-02-16 22:52:38 +0800

    feat: 项目初始化.

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import AppWithNavigationState  from './src/navigator/AppNavigators'; // 入口文件
+import AppWithNavigationState from './src/navigator/AppNavigators'; // 入口文件
 
 export default function App() {
   return (
-    <AppWithNavigationState />
-
+    <AppWithNavigationState/>
   );
 }
